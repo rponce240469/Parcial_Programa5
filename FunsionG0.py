d47fcd47163ff022bc498a01ad154782c8ef6a6e
Ronald Isaac Ponce <geosist5@gmail.com>
2026-05-31 18:40:01 -0500

    Create FunsionG0.py
    Primer commit realizado por el profesor Ronald

---
+++ FunsionG0.py
@@ -0,0 +1,9 @@
+# par-Impar – Mayor-numero
+#*****************************************
+#**    Desarrollado por: Hector Lavoe   **
+#*****************************************
+# inicia desarrollo de la funciín dada
+
+# cuando imprime resultado
+# en el print "Su nommbre y la salida del resultado de la función"
+# compruebe buen funcionamiento
