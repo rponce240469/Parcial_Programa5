088d764f9dfe383c70d98d900b3e7c9f3029168a
Melany Rodriguez <mel0021r@gmail.com>
2026-06-01 18:24:24 -0500

    Parte del codigo del grupo 4

--- FunsionG0.py
+++ FunsionG0.py
@@ -7,3 +7,23 @@
 # cuando imprime resultado
 # en el print "Su nommbre y la salida del resultado de la función"
 # compruebe buen funcionamiento
+
+
+# Par 4 – FunsionG4.py
+# Lista-Número – Lista-Contenido
+# *****************************************
+# Estudiante 1: Juan Miranda
+# *****************************************
+
+def cantidad_elementos(lista):
+    return len(lista)
+
+print("Estudiante 1:", cantidad_elementos([1, 2, 3, 4, 5]))
+
+
+# Estudiante 2: Melany Rodriguez
+
+def valor_minimo(lista):
+    return min(lista)
+
+print("Estudiante 2:", valor_minimo([8, 3, 12, 1, 9]))
