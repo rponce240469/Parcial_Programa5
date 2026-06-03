38d204a6e25b2706101bdebe0dd8449180e140d4
Roger Herrera <roger.herrera062005@gmail.com>
2026-06-02 19:32:41 -0500

    Agregadas funciones Grupo 1

--- FunsionG0.py
+++ FunsionG0.py
@@ -27,3 +27,27 @@ def valor_minimo(lista):
     return min(lista)
 
 print("Estudiante 2:", valor_minimo([8, 3, 12, 1, 9]))
+
+#***************************************
+# Desarrollado por: Roger Herrera
+#***************************************
+
+def par_impar(numero):
+    if numero % 2 == 0:
+        return "Par"
+    else:
+        return "Impar"
+
+print("Roger Herrera:", par_impar(8))
+
+
+#****Desarrollado por Meylin Hill****
+
+def mayor_numero(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+print("Meylin Hill:", mayor_numero(10, 25))
+
